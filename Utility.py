889da1bc6d1bb41b7017b928ca20466263ccacd5
Emilie-perso <emilielalou974@gmail.com>
2022-09-16 18:42:42 +0200

    fetch data from API

--- Utility.py
+++ Utility.py
@@ -1,5 +1,10 @@
 import pandas as pd 
 import requests 
+#declaration variables 
+url =  "https://opendata.paris.fr/api/records/1.0/search/?dataset=chantiers-perturbants&q=&rows=4&facet=cp_arrondissement&facet=typologie&facet=maitre_ouvrage&facet=objet&facet=impact_circulation&facet=niveau_perturbation&facet=statut&facet=description"
 
-url =  "https://opendata.paris.fr/api/records/1.0/search/?dataset=chantiers-perturbants&q=&rows=400&facet=cp_arrondissement&facet=typologie&facet=maitre_ouvrage&facet=objet&facet=impact_circulation&facet=niveau_perturbation&facet=statut&facet=description"
-
+response = requests.get(url)
+response.raise_for_status()
+contenu = response.json()
+records = contenu["records"]
+df = pd.DataFrame.from_dict(records)
