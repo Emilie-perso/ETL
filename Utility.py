848d795625e150ec80bb974d05c21a65ac222005
Emilie-perso <emilielalou974@gmail.com>
2022-09-16 18:11:00 +0200

    setting up environnement

---
+++ Utility.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+import requests 
+
+url =  "https://opendata.paris.fr/api/records/1.0/search/?dataset=chantiers-perturbants&q=&rows=400&facet=cp_arrondissement&facet=typologie&facet=maitre_ouvrage&facet=objet&facet=impact_circulation&facet=niveau_perturbation&facet=statut&facet=description"
+
