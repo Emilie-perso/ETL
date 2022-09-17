305315c4078f178d7fe049d4f1cfc6653da9ef7d
Emilie-perso <emilielalou974@gmail.com>
2022-09-17 11:01:22 +0200

    Load to CSV file

--- Utility.py
+++ Utility.py
@@ -2,7 +2,7 @@ from dataclasses import field
 import pandas as pd 
 import requests 
 #declaration variables 
-url =  "https://opendata.paris.fr/api/records/1.0/search/?dataset=chantiers-perturbants&q=&rows=4&facet=cp_arrondissement&facet=typologie&facet=maitre_ouvrage&facet=objet&facet=impact_circulation&facet=niveau_perturbation&facet=statut&facet=description"
+url =  "https://opendata.paris.fr/api/records/1.0/search/?dataset=chantiers-perturbants&q=&rows=400&facet=cp_arrondissement&facet=typologie&facet=maitre_ouvrage&facet=objet&facet=impact_circulation&facet=niveau_perturbation&facet=statut&facet=description"
 
 # fetch data 
 response = requests.get(url)
@@ -19,3 +19,6 @@ dict_values = dict_fields.values()
 
 df_final = pd.DataFrame.from_dict(dict_values)
 
+#Load
+
+df_final.to_csv('travaux_perturbants_la_circulation.csv', index=False)
